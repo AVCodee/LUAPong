5b03650ef5e22ccff859cd9f0cce94ee9fd561ef
Alvyn Velazquez <alvyn1010@gmail.com>
2021-06-20 17:21:33 -0400

    Refactored Ball Collision Code for Readability

--- Pong/ball.lua
+++ Pong/ball.lua
@@ -16,22 +16,13 @@ function Ball:update(dt)
 end
 
 function Ball:collide()
-    if checkCollision(self, Player) then
-        self.xVel = self.speed
-        local middleBall = self.y + self.height / 2
-        local middlePlayer = Player.y + Player.height / 2
-        local collisionPosition = middleBall - middlePlayer
-        self.yVel = collisionPosition * 5
-    end
+    self:collideWall()
+    self:collidePlayer()
+    self:collideAI()
+    self:score()
+end
 
-    if checkCollision(self, AI) then
-        self.xVel = -self.speed
-        local middleBall = self.y + self.height / 2
-        local middleAI = AI.y + AI.height / 2
-        local collisionPosition = middleBall - middleAI
-        self.yVel = collisionPosition * 5
-    end
-
+function Ball:collideWall()
     if self.y < 0 then
         self.y = 0
         self.yVel = -self.yVel
@@ -39,21 +30,54 @@ function Ball:collide()
         self.y = love.graphics.getHeight() - self.height
         self.yVel = -self.yVel
     end 
+end
 
-    if self.x < 0 then 
-        self.x = love.graphics.getWidth() / 2 - self.width / 2
-        self.y = love.graphics.getHeight() / 2 - self.height / 2
-        self.yVel = 0 
+
+function Ball:collidePlayer()
+    if checkCollision(self, Player) then
         self.xVel = self.speed
+        local middleBall = self.y + self.height / 2
+        local middlePlayer = Player.y + Player.height / 2
+        local collisionPosition = middleBall - middlePlayer
+        self.yVel = collisionPosition * 5
+    end
+end
+
+
+
+function Ball:collideAI()
+    if checkCollision(self, AI) then
+        self.xVel = -self.speed
+        local middleBall = self.y + self.height / 2
+        local middleAI = AI.y + AI.height / 2
+        local collisionPosition = middleBall - middleAI
+        self.yVel = collisionPosition * 5
+    end
+end
+
+
+
+function Ball:score()
+    if self.x < 0 then 
+       self:resetPosition(1)
     end 
 
     if self.x + self.width > love.graphics.getWidth() then 
-        self.x = love.graphics.getWidth() / 2 - self.width / 2
-        self.y = love.graphics.getHeight() / 2 - self.height / 2
-        self.yVel = 0 
-        self.xVel = -self.speed
+        self:resetPosition(-1)
     end 
 end
+
+
+
+function Ball:resetPosition(mod)
+    self.x = love.graphics.getWidth() / 2 - self.width / 2
+    self.y = love.graphics.getHeight() / 2 - self.height / 2
+    self.yVel = 0 
+    self.xVel = self.speed * mod
+
+end
+
+
 
 function Ball:move(dt)
     self.x = self.x + self.xVel * dt
