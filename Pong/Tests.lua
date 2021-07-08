2fe1aed7b2de2df775b682fe1dc336954496836e
Alvyn Velazquez <alvyn1010@gmail.com>
2021-07-08 07:54:40 -0400

    Additional Changes to Pickup Item

--- Pong/Tests.lua
+++ Pong/Tests.lua
@@ -1,22 +1,34 @@
 luaunit = require('luaunit')
-os.exit( luaunit.LuaUnit.run() )
-require("main")
 
 
-function TestItem:load()
-    self.x = love.graphics.getWidth() / 2
-    self.y = love.graphics.getHeight() / 2
-    self.width = 20
-    self.height = 20
-    self.speed = 100
+
+
+function checkCollision(a, b) -- Add Unit Test Cases for proper testing
+    a.x = 500
+    a.y = 500
+    a.width = 300
+    a.height = 300
+
+    b.x = 200
+    b.y = 200
+    b.width = 400
+    b.height = 400
+    if a.x + a.width > b.x and a.x < b.x + b.width and a.y + a.height > b.y and a.y < b.y + b.height then
+        return true
+    else 
+        return false
+    end 
+
 end
 
 
-
 function testCollision(a, b)
+   
     luaunit.assertEquals(checkCollision(a, b), true)
 end
 
 function testNoCollision(a, b)
     luaunit.assertEquals(checkCollision(a, b), false)
 end
+
+os.exit( luaunit.LuaUnit.run() )
